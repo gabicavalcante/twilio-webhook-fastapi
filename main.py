e6ae816ecf87d8885d18fca673adca92d84b3e8f
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2020-04-24 23:32:54 -0300

    add endpoint showing how to not use request

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import os
-from fastapi import FastAPI, Form, Request, HTTPException, Response
+from fastapi import FastAPI, Form, Request, HTTPException, Response, Header, Path
 from twilio.twiml.messaging_response import MessagingResponse
-from twilio.request_validator import RequestValidator 
+from twilio.request_validator import RequestValidator
 
-app = FastAPI() 
+app = FastAPI()
+
 
 @app.post("/hook")
 async def chat(From: str = Form(...), Body: str = Form(...)):
-   response = MessagingResponse() 
-   response.message(f"Hi {From}, you said: {Body}")
-   return Response(content=str(response), media_type="application/xml")
+    response = MessagingResponse()
+    response.message(f"Hi {From}, you said: {Body}")
+    return Response(content=str(response), media_type="application/xml")
 
 
-@app.post("/security/hook")
+@app.post("/hook2")
 async def security_chat(request: Request):
     validator = RequestValidator(os.environ["TWILIO_AUTH_TOKEN"])
 
-    form_ = await request.form() 
+    form_ = await request.form()
 
-    print(form_.items())
+    print(request.url)
 
     if not validator.validate(
         str(request.url), dict(form_.items()), request.headers["X-Twilio-Signature"]
@@ -27,6 +28,24 @@ async def security_chat(request: Request):
 
     Body = form_.get("Body")
     From = form_.get("From")
+
+    response = MessagingResponse()
+    response.message(f"Hi {From} | {Body}")
+    return Response(content=str(response), media_type="application/xml")
+
+
+@app.post("/hook3")
+async def security_chat_header(
+    From: str = Form(...), Body: str = Form(...), x_twilio_signature: str = Header(None)
+):
+    validator = RequestValidator(os.environ["TWILIO_AUTH_TOKEN"])
+
+    if not validator.validate(
+        f"{os.environ['API_HOST']}/hook",
+        {"From": From, "Body": Body},
+        x_twilio_signature,
+    ):
+        raise HTTPException(status_code=400, detail="Error Twilio Signature")
 
     response = MessagingResponse()
     response.message(f"Hi {From} | {Body}")
