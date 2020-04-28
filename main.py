aded46ea0e3ed9168f712fb18e19324c603dcb84
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2020-04-27 21:35:36 -0300

    update security hook

--- main.py
+++ main.py
@@ -13,37 +13,14 @@ async def chat(From: str = Form(...), Body: str = Form(...)):
     return Response(content=str(response), media_type="application/xml")
 
 
-@app.post("/hook2")
-async def security_chat(request: Request):
-    validator = RequestValidator(os.environ["TWILIO_AUTH_TOKEN"])
-
-    form_ = await request.form()
-
-    print(request.url)
-
-    if not validator.validate(
-        str(request.url), dict(form_.items()), request.headers["X-Twilio-Signature"]
-    ):
-        raise HTTPException(status_code=400, detail="Error Twilio Signature")
-
-    Body = form_.get("Body")
-    From = form_.get("From")
-
-    response = MessagingResponse()
-    response.message(f"Hi {From} | {Body}")
-    return Response(content=str(response), media_type="application/xml")
-
-
-@app.post("/hook3")
-async def security_chat_header(
-    From: str = Form(...), Body: str = Form(...), x_twilio_signature: str = Header(None)
-):
+@app.post("/security/hook/")
+async def security_chat(request: Request, From: str = Form(...), Body: str = Form(...)):
     validator = RequestValidator(os.environ["TWILIO_AUTH_TOKEN"])
 
     if not validator.validate(
-        f"{os.environ['API_HOST']}/hook",
+        str(request.url),
         {"From": From, "Body": Body},
-        x_twilio_signature,
+        request.headers.get("X-Twilio-Signature", ""),
     ):
         raise HTTPException(status_code=400, detail="Error Twilio Signature")
 
